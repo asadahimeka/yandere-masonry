84d6438a96194f56016f70fc5191072a543c8f0a
asadahimeka <smallstar9637@yahoo.co.jp>
2022-05-18 01:24:27 +0800

    release 0.1.3

--- push.mjs
+++ push.mjs
@@ -1,14 +1,19 @@
 import { readFile } from 'node:fs/promises'
 import { execSync } from 'node:child_process'
 
+function exec(cmd)  {
+  const buf = execSync(cmd)
+  console.log(buf.toString())
+}
+
 async function push() {
   const pkgStr = await readFile('./package.json', 'utf8')
   const { version } = JSON.parse(pkgStr)
-  execSync('yarn build')
-  execSync('git add .')
-  execSync(`git commit -m "release ${version}"`)
-  execSync('set https_proxy=http://127.0.0.1:1081')
-  execSync('git push')
+  exec('yarn build')
+  exec('git add .')
+  exec(`git commit -m "release ${version}"`)
+  exec('set https_proxy=http://127.0.0.1:1081')
+  exec('git push')
 }
 
 push()
