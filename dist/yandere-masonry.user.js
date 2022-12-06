55b0f566ab8fc8236bde90b0327aae29886969ef
asadahimeka <smallstar9637@yahoo.co.jp>
2022-12-06 20:37:37 +0800

    release: v0.27.5

--- dist/yandere-masonry.user.js
+++ dist/yandere-masonry.user.js
@@ -2,7 +2,7 @@
 // @name                 Yande.re 瀑布流浏览
 // @name:en              Yande.re Masonry
 // @name:zh              Yande.re 瀑布流浏览
-// @version              0.27.4
+// @version              0.27.5
 // @description          Yande.re/Konachan 中文标签 & 缩略图放大 & 双击翻页 & 瀑布流浏览模式(支持 danbooru/gelbooru/rule34/sakugabooru/lolibooru/safebooru/3dbooru/xbooru/atfbooru/aibooru 等)
 // @description:en       Yande.re/Konachan Masonry(Waterfall) Layout. Also support danbooru/gelbooru/rule34/sakugabooru/lolibooru/safebooru/3dbooru/xbooru/atfbooru/aibooru et cetera.
 // @description:zh       Yande.re/Konachan 中文标签 & 缩略图放大 & 双击翻页 & 瀑布流浏览模式(支持 danbooru/gelbooru/rule34/sakugabooru/lolibooru/safebooru/3dbooru/xbooru/atfbooru/aibooru 等)
@@ -124,6 +124,8 @@ var __publicField = (obj, key, value) => {
     (_a2 = wfTypeActions[wfType]) == null ? void 0 : _a2.call(wfTypeActions, listEl);
   }
   function addWfTypeSelect() {
+    if (!location.href.includes("yande.re/post"))
+      return;
     const params2 = new URLSearchParams(location.search);
     if (params2.get("_wf"))
       return;
